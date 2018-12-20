47be33f9c4ad050b6990c276f175d5c60f9f5cea
a.bilanenka <a.bilanenka@intetics.com>
2018-12-20 19:23:51 +0300

    444

---
+++ OOProject/OOProject/ConsolePrinter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProject
+{
+    class ConsolePrinter : IPrinter
+    {
+<<<<<<< HEAD
+        public void Print(IEnumerable collection)
+        {
+            Console.WriteLine(Environment.NewLine + "Vector elements:");
+            foreach (var el in collection)
+            {
+                Console.Write(el + " ");
+=======
+        public void PrintHeader(string name)
+        {
+            Console.WriteLine($"Vector {name} elements:");
+        }
+
+        public void Print(IEnumerable collection)
+        {
+            foreach (var el in collection)
+            {
+                Console.WriteLine(el);
+>>>>>>> parent of dbc8a78... Merge branch 'RefactoringBranch'
+            }
+        }
+    }
+}
